7cb55ae4a728831433792304c078a10618b2a605
basakbilginoglu <basakbilginoglu@gmail.com>
2025-12-30 16:55:12 +0300

    fetching meal data,get http request process added

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,13 @@
 import Header from './components/Header.jsx';
+import Meals from './components/Meals.jsx';
+
 
 function App() {
   return (
-   <Header/>
-
+  <> 
+   <Header />
+   <Meals />
+  </> 
   );
 }
 

f990d3a39b3d3699ec7af54bcc7cb7b502dd0e35
basakbilginoglu <basakbilginoglu@gmail.com>
2026-01-02 16:08:47 +0300

    CartContext and Reduceradded and ADD_ITEM situation was wrote

---
+++ src/store/CartContext.jsx
@@ -0,0 +1,48 @@
+import { createContext,useReducer } from "react";
+
+const CartContext=createContext(
+    {
+        items:[],
+        addItem: (item) => {},
+        removeItem: (id) => {}
+    }
+);
+
+function cartReducer(state,action){
+    if(action.type==='ADD_ITEM'){
+       const existingItemIndex=state.items.findIndex(item=>item.id===action.item.id)
+       
+       const updatedItems = [...state.items];
+       if (existingItemIndex>-1){
+        const existingItem=state.items[existingItemIndex];
+     const updatedItem={
+       ...existingItem,
+        quantity:existingItem.quantity + 1
+     };
+      updatedItems[existingItemIndex]=updatedItem;
+     }        
+       }else{
+        updatedItems.push({...action.item,quantity:1});
+       }
+       return {
+        items:updatedItems
+
+        }
+             
+    }
+
+    if(action.type==='REMOVE_ITEM'){
+
+    }   
+    return state;
+}   
+
+export function CartContextProvider({children}){
+    useReducer(cartReducer,{
+        items:[]
+    });
+    return <CartContext.Provider>{children}</CartContext.Provider>
+
+}
+
+export default CartContext;

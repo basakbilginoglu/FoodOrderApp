bcd0154e72521b4997eb8ac0b14cc7f1bc9f90e4
basakbilginoglu <basakbilginoglu@gmail.com>
2026-01-02 19:10:08 +0300

    Cart modal first steps added(CreatePortal useRef was used)

---
+++ src/components/UI/Modal.jsx
@@ -0,0 +1,17 @@
+import React from 'react'
+import { createPortal } from 'react-dom'
+
+export default function Modal({children,open,className=''}) {
+
+    const dialogRef = useRef()
+    useEffect(()=>{
+        if(open){
+            dialogRef.current.showModal()
+        }
+    },
+    [open])
+  return createPortal ( <dialog ref={dialogRef}className={`modal ${className}`}>{children}</dialog>, document.getElementById('modal')
+      
+   
+  )
+}
